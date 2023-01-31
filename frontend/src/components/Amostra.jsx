96233eec8988111ec771c1fbd0ee91707f9e7ccb
Heitor <dev04@nk.local>
2023-01-31 13:54:33 -0300

    add page Amostra

--- frontend/src/components/Amostra.jsx
+++ frontend/src/components/Amostra.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
+import {
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Row,
+    Col,
+    Label,
+    FormGroup,
+    Collapse,
+    FormFeedback,
+    Modal,
+    Input,
+    Button as ButtonCartao
+} from "reactstrap";
+import { TreeSelect, Tooltip, Upload, Alert, Button, Select, Table, Tag, DatePicker, Spin, Form, Switch, Radio, Skeleton } from 'antd';
+
+
+const styleLabel = { fontWeight: "600" }
+const inputObrigatorio = <i style={{color: "red", fontWight: "Bold"}}> *</i>
 
 const Amostra = () => {
 return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>Cadastro de amostras do solo</h1>
+	<div>
+		<>
+
+		<Form>
+		<Row>
+		<Col md='8'>
+		<FormGroup>
+		<Label>aaa</Label>
+		<Input>aaa</Input>
+		</FormGroup>
+		</Col>
+		</Row>
+		</Form>
+		
+
+		<Label>aaa</Label>
+		<Input>aaa</Input>
+
+		<Label>aaa</Label>
+		<Input>aaa</Input>
+
+		<Label>aaa</Label>
+		<Input>aaa</Input>
+
+		<Label>aaa</Label>
+		<Input>aaa</Input>
+	</>
 	</div>
+	
 );
 };
 
