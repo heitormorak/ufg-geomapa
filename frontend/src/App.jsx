cef973eec2e8108e4ccc05b8d3fae95d8f11b745
Heitor <dev04@nk.local>
2023-02-07 14:04:22 -0300

    add fonts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/navbar/Navbar"
-import Mapa from "./components/Mapa"
+// import Mapa from "./components/Mapa"
 import Equipe from "./components/Equipe"
 import Amostra from "./components/Amostra"
 
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/Mapa' element={<Mapa/>} />   
+        {/* <Route path='/Mapa' element={<Mapa/>} />    */}
         <Route path='/Equipe' element={<Equipe/>}/>     
         <Route path='/Amostra' element={<Amostra/>} />  
       </Routes>
