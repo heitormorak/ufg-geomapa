3dc1dd8897e2c1961fb25a2aa2a816a2aa109e6f
unknown <heitormorak@gmail.com>
2023-02-07 22:41:08 -0300

    add function Salvar

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,15 +1,19 @@
 const SearchBar = () => (
     <form action="/" method="get">
         <label htmlFor="header-search">
-            <span className="visually-hidden">Search blog posts</span>
+            <span className="visually-hidden" style={{fontFamily: 'tisa_sans_probold', textAlign:'start', fontSize:'18px'}}>Pesquisar relatório por número</span>
         </label>
         <input
             type="text"
             id="header-search"
-            placeholder="Search blog posts"
+            placeholder="Digite o número do relatório"
             name="s" 
+            style={{fontFamily: 'tisa_sans_probold', width: '100%',  padding: '10px', border: 'none', marginBottom: '10px', borderRadius: '5px'}}
         />
-        <button type="submit">Search</button>
+        <button type="submit"
+                 style={{fontFamily: 'tisa_sans_probold', borderRadius: '5px'}}
+                >Procurar
+        </button>
     </form>
 );
 
